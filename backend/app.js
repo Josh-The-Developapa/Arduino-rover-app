5ea7af466a2e54160c694003f41ac07f5dc87b84
Joshua Mukisa <kiryowajoshua22@gmail.com>
2022-10-22 13:56:20 +0300

    Committed

--- backend/app.js
+++ backend/app.js
@@ -9,41 +9,72 @@ const io = new Server(server, {
     }
 });
 
-const { Board, Motor } = require("johnny-five");
+//Requiring the Board and Motor classes from johnny-five library
+const { Board, Motor, Led } = require("johnny-five");
 
+//Create a new board instance
 const board = new Board();
 
+//Wait for board to finish setting up
 board.on("ready", () => {
+    //Create instances for the two motors (a and b) and store them in an object
     const motors = {
+        //Motor 'a' created using Motor class that takes in a numbers array of the pins its been connected to (2 and 3).
         a: new Motor([2, 3]),
+        //Motor 'b' created using Motor class that takes in a numbers array of the pins its been connected to (4 and 5).
         b: new Motor([4, 5])
     }
 
+    //Create instance for in built LED light on pin 13
+    const led = new Led(13)
+
+    //Start listening for a connection event using socket.io bi-directional communication protocol.
     io.on('connection', (socket) => {
+        //Log to the console when a user has connected
         console.log('User connected!')
 
+        socket.on('turn-on', function () {
+            led.on()
+        })
+
+        socket.on('turn-off', function () {
+            led.off()
+        })
+
+        //Listen for when 'forward' event is fired 
         socket.on('forward', function (speed) {
+            //Rotate both motors clockwise at speed in the payload sent with event
             motors.a.forward(speed);
             motors.b.forward(speed);
         })
 
+        //Listen for when 'reverse' event is fired 
         socket.on('reverse', function (speed) {
+            //Rotate both motors anti-clockwise at speed in the payload sent with event
             motors.a.reverse(speed);
             motors.b.reverse(speed);
         })
 
+        //Listen for when 'left' event is fired
         socket.on('left', function (speed) {
+            //Rotate motor 'a' anti-clockwise at speed in the payload sent with event
             motors.a.reverse(speed);
+            //Rotate motor 'b' clockwise at speed in the payload sent with event
             motors.b.forward(speed);
         })
 
+        //Listen for when 'right' event is fired
         socket.on('right', function (speed) {
+            //Rotate motor 'a' clockwise at speed in the payload sent with event
             motors.a.forward(speed);
+            //Rotate motor 'b' anti-clockwise at speed in the payload sent with event
             motors.b.reverse(speed);
         })
 
+        //Listen for when 'stop' event is fired
         socket.on('stop', function () {
-            motors.a.stop()
+            //Stop motion for both motors
+            motors.a.stop();
             motors.b.stop();
         })
 
@@ -52,6 +83,7 @@ board.on("ready", () => {
 
 const PORT = 8000;
 
+//Start the server listening on port: 8000 stored in 'PORT' variable
 server.listen(PORT, () => {
     console.log(`Listening on port: ${ PORT }`);
 });
