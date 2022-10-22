740cb35aa5c364345b1b9e9409e7ba33cae51afc
Joshua Mukisa <kiryowajoshua22@gmail.com>
2022-10-22 13:56:43 +0300

    Committed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import img from './assets/arduino-icon-2.png'
 import io from 'socket.io-client'
@@ -7,28 +7,49 @@ const socket = io('http://localhost:8000');
 
 function App() {
   const [speed, setSpeed] = useState(0);
+  const [on, setOn] = useState(false);
   return (
     <div className='outerc'>
-      {/* <h1>Remote controlled</h1> */}
       <h1> Arduino rover</h1>
       <div className='card'>
         <div>
-          {/* <h1> Arduino Rover controller</h1> */}
           <div className="container">
-            {/* <h1 style={{ position: 'relative', top: '-50px' }}> Arduino Rover controller</h1> */}
             <img src={img} alt='arduino-logo' style={{ position: 'relative', top: '-22px', left: '92px' }} />
+            {/* <div style={{ display: 'flex', flexDirection: 'column', height: '100px', width: '200px', justifyContent: 'center', alignItems: 'center', backgroundColor: '#145b4d' }}> */}
+            {/* <h3 style={{ marginBottom: '10px' }}>Turn on LED</h3> */}
+            <label class="switch" style={{ marginTop: '10px' }}>
+              <input type="checkbox" value={on} onClick={() => {
+                if (on) {
+                  setOn(false)
+                  socket.emit('turn-off')
+                } else {
+                  setOn(true)
+                  socket.emit('turn-on')
+                }
+              }} />
+              <span class="slider round"></span>
+            </label>
+            {/* </div> */}
             <i className="fa fa-angle-up" id="forward" onClick={() => {
               if (speed > 0) {
                 socket.emit('forward', speed)
               }
             }}></i>
-            <i className="fa fa-angle-left" id="left"></i>
+            <i className="fa fa-angle-left" id="left" onClick={() => {
+              if (speed > 0) {
+                socket.emit('left', speed)
+              }
+            }}></i>
             <i className="fa fa-angle-down" id="reverse" onClick={() => {
               if (speed > 0) {
                 socket.emit('reverse', speed)
               }
             }}></i>
-            <i className="fa fa-angle-right" id="right"></i>
+            <i className="fa fa-angle-right" id="right" onClick={() => {
+              if (speed > 0) {
+                socket.emit('right', speed)
+              }
+            }}></i>
             <i className="fa stop" id="stop" onClick={() => {
               socket.emit('stop');
             }}>Stop</i>
@@ -36,7 +57,7 @@ function App() {
         </div>
       </div>
       <label htmlFor='speed'>Speed:</label>
-      <input type='range' style={{ cursor: 'pointer' }} step='100' min='0' max='400' value={speed} onChange={(e) => {
+      <input type='range' style={{ cursor: 'pointer' }} step='100' min='0' max='500' value={speed} onChange={(e) => {
         setSpeed(e.target.value)
         console.log(e.target.value)
       }} />
